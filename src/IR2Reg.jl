1f22162bcde3a4471f333e719f50af4c473cd91d
nathanemac <91251698+nathanemac@users.noreply.github.com>
2024-09-24 09:43:28 -0400

    add main files to src and update dependencies

--- src/IR2Reg.jl
+++ src/IR2Reg.jl
@@ -1,11 +1,22 @@
 module IR2Reg
 
-"""
-    hi = hello_world()
-A simple function to return "Hello, World!"
-"""
-function hello_world()
-    return "Hello, World!"
+using ADNLPModels
+using LinearAlgebra
+using NLPModels
+using Printf
+using ProxTV
+using ProximalOperators
+using RegularizedOptimization
+using ShiftedProximalOperators
+using SolverCore
+
+
+function __init__()
+    #empty for now
 end
+
+# main library functions
+include("ir2reg_algo.jl")
+include("utils.jl")
 
 end
